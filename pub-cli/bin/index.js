f7e89ce7c1819b81f96e7af56cecd40b16bea0e2
Alexandre MEUR <alex.meur@gmail.com>
2019-06-19 14:45:07 +0200

    Client fini

--- pub-cli/bin/index.js
+++ pub-cli/bin/index.js
@@ -5,7 +5,15 @@ var program = require('commander');
 program
     .version('1.0.0')
     .option('-l, --list', 'Get the pubs list')
+    .option('-o, --open', 'Get the pubs open today')
     .parse(process.argv);
 if (program.list) {
-    require('../src/main').listPubs();
+    let listPubs = require('../src/main').listPubs;
+    console.log(listPubs());
+}
+else if(program.open){
+    let pubsOpenOnDay = require('../src/main').pubsOpenOnDay;
+    let days = ['Sunday', 'Monday', 'Thursday', 'Wednesday', 'Tuesday', 'Friday', 'Saturday'];
+    let dayNb = new Date().getDay();
+    console.log(pubsOpenOnDay(days[dayNb]));
 }
--- pub-services/services/pubs.service.js
+++ pub-services/services/pubs.service.js
@@ -1,14 +1,11 @@
 
+var JsonTab = require('../mocks/pubs');
 /**
  * Liste tous les pubs par leurs noms
  * @param {Le tableau des pubs en Json} JsonData 
  */
-function listPubs(JsonTab) {
-    var listPubs = new Array(JsonTab.length);
-    for (var i = 0; i < JsonTab.length; i++) {
-        listPubs[i] = JsonTab[i].name;
-    }
-    return listPubs;
+function listPubs() {
+    return JsonTab.map(pub => pub.name);
 }
 
 /**
@@ -16,14 +13,8 @@ function listPubs(JsonTab) {
  * @param {Le jour} day 
  * @param {Le tableau des pubs} JsonTab 
  */
-function pubsOpenOnDay(day, JsonTab) {
-    var result = new Array();
-    for (var i = 0; i < JsonTab.length; i++) {
-        if (JsonTab[i].openDays.includes(day)) {
-            result.push(JsonTab[i].name);
-        }
-    }
-    return result;
+function pubsOpenOnDay(day) {
+    return JsonTab.filter(pub => pub.openDays.includes(day)).map(pub => pub.name);
 }
 
 exports.pubsOpenOnDay = pubsOpenOnDay;
