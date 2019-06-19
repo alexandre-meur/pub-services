07e665d26601e114a4300fb0782408c6a173964e
Alexandre MEUR <alex.meur@gmail.com>
2019-06-19 10:12:50 +0200

    Changement de la structure des dossiers

---
+++ pub-cli/bin/index.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+var program = require('commander');
+
+program
+    .version('1.0.0')
+    .option('-l, --list', 'Get the pubs list')
+    .parse(process.argv);
+if (program.list) {
+    require('../src/main').listPubs();
+}
